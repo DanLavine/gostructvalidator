e912856b944c42a41bec85caba441217aef2aa8b
Dan Lavine <lavineds@gmail.com>
2022-09-08 14:20:41 -0700

    have things working, but need a nice error handler

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # gostructvalidator
+
+A fully extendable genric validat tool for any struct in golang.
+
+
+# TODO
+  1. Come up with nice error handling
+  2. Have nice struct tags that handle arrays, slices, maps
+  3. provide a list of common validators
+  4. Handle pointers sainly for everyone
--- validators/min_length.go
+++ validators/min_length.go
@@ -2,13 +2,22 @@ package validators
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/DanLavine/gostructwalker"
 )
 
-func MinLength(f *gostructwalker.StructParser, tag string) error {
+func MinLength(structParser *gostructwalker.StructParser, tagValue string) error {
+	minLength, err := strconv.Atoi(tagValue)
+	if err != nil {
+		return err
+	}
 
-	fmt.Println("In min length")
+	fmt.Println(minLength)
+
+	if structParser.Value.Len() < minLength {
+		return fmt.Errorf("value '%v' is less than min legth %d", structParser.Value.Interface(), minLength)
+	}
 
 	return nil
 }
